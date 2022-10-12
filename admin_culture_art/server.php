6ee24e0c5c771a8b8af207608111ef3d42a9abcc
Krupanshifaldu <110378462+Krupanshifaldu@users.noreply.github.com>
2022-10-12 11:41:27 +0530

    Delete admin_culture_art directory

--- admin_culture_art/server.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-// initializing variables
-$username = "";
-$email    = "";
-$errors = array();
-// connect to the database
-$db = mysqli_connect('localhost', 'root', '', 'culture');
